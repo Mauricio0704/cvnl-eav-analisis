7666c0012b3637362ffd68f6e100f4a4d191d49c
Mauricio <mausancarr0704@gmail.com>
2025-12-19 16:04:07 -0600

    Set up intial structure

---
+++ src/ingestion/load_data.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import pandas as pd
+
+
+def load_survey_year(year: int, base_dir: Path) -> pd.DataFrame:
+    file_path = base_dir / "raw" / str(year) / "data_eav2025-raw.xlsx"
+
+    if not file_path.exists():
+        raise FileNotFoundError(f"Survey file not found for year {year}")
+
+    df = pd.read_excel(file_path)
+
+    return df
+
+
+def load_questions_year(year: int, base_dir: Path) -> pd.DataFrame:
+    file_path = base_dir / "raw" / str(year) / "cuestionario-eav2025.xlsx"
+
+    if not file_path.exists():
+        raise FileNotFoundError(f"Questions file not found for year {year}")
+
+    df = pd.read_excel(file_path)
+
+    return df
