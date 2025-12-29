86339c89cecaffbd9c02e135ab928ecb18bf46cd
Mauricio0704 <mausancarr0704@gmail.com>
2025-12-29 11:47:24 -0600

    Add scripts for database loading and data ingestion

---
+++ scripts/run_ingestion.py
@@ -0,0 +1,60 @@
+from src.ingestion.load_data import load_survey_year, load_questions_year
+from src.mapping.extract_household_block import get_household_data
+from src.mapping.extract_individual_block import get_individual_responses
+from src.cleaning.transform_structure import (
+    household_members_to_long_format,
+    household_questions_to_long_format,
+    individual_questions_to_long_format,
+)
+from src.cleaning.clean_household import clean_household_responses
+from src.cleaning.clean_survey import clean_questions, clean_options, clean_responses
+from src.mapping.questions_shema import normalize_questions_schema
+from src.io.export_to_csv import (
+    export_responses_to_csv,
+    export_questions_to_csv,
+    export_options_to_csv,
+    export_household_answers_to_csv,
+    export_individual_answers_to_csv,
+)
+from src.config.paths import DATA_DIR
+from src.config.survey_data import DEMOGRAPHIC_CODES
+
+
+def main():
+    year = 2025
+    base_dir = DATA_DIR
+
+    print("Loading raw survey data...")
+    survey_df = load_survey_year(year, base_dir)
+
+    print("Processing data...")
+
+    # Household responses
+    household_df = get_household_data(survey_df)
+    household_members_lf = household_members_to_long_format(household_df, 12)
+    household_clean = clean_household_responses(household_members_lf)
+    household_questions_lf = household_questions_to_long_format(household_clean)
+    responses_clean = clean_responses(household_clean, DEMOGRAPHIC_CODES)
+    export_household_answers_to_csv(household_questions_lf)
+    export_responses_to_csv(responses_clean)
+
+    # Individual responses
+    individual_df = get_individual_responses(survey_df)
+    individual_lf = individual_questions_to_long_format(individual_df)
+    export_individual_answers_to_csv(individual_lf)
+
+    # Questions and options
+    questions_raw = load_questions_year(year, base_dir)
+    questions_norm = normalize_questions_schema(questions_raw)
+
+    questions_clean = clean_questions(questions_norm)
+    options_clean = clean_options(questions_norm)
+
+    export_questions_to_csv(questions_clean)
+    export_options_to_csv(options_clean)
+
+    print("Ingestion completed.")
+
+
+if __name__ == "__main__":
+    main()
